f63fbb090c0cf0391f39952edc835d9afa7d49f7
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-09-16 04:18:59 +0530

    adding changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const userSchema = new mongoose.Schema();
-
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
