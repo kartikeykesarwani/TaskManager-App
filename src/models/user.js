5d9b8d3f933dc1dbb86834ed8bf510734c22287a
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-09-21 20:50:16 +0530

    commiting changes

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -95,6 +96,17 @@ userSchema.pre("save", async function (next) {
   }
 
   next();
+});
+
+//Delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+});
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
 });
 
 const User = mongoose.model("User", userSchema);
