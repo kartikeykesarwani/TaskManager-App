541531d335908b474a2c654dea9424dafff57a39
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-09-22 21:01:30 +0530

    commiting changes

--- src/index.js
+++ src/index.js
@@ -15,14 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const main = async () => {
-  //   const task = await Task.findById("5f68a6b637818d02d0c44074");
-  //   await task.populate("owner").execPopulate();
-  //   console.log(task.owner);
-  const user = await User.findById("5f68a63b45481e7d5cec388b");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-
-main();
