2a343edd7155267a9728359c5efb6f1f51229c43
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-09-25 04:15:41 +0530

    commiting changes

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,9 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
+    avatar: {
+      type: Buffer,
+    },
   },
   {
     timestamps: true,
@@ -63,6 +66,7 @@ userSchema.methods.toJSON = function () {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
 
   return userObject;
 };
